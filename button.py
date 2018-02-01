cbb5eaf28c645ef72f830415fe9b2fe1c8bb4fec
Stuart Ellis <stuart@stuartellis.name>
2018-02-01 19:13:08 +0000

    Amend script for pylint compliance

--- button.py
+++ button.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python
+
+"""
+Button provides a convenient way to run an AWS CloudFormation template.
+"""
+
+import subprocess
+
+def main():
+    """
+    Main function
+    """
+    pass
+
+if "__name__" == "__main__":
+    main()
